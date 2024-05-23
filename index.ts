f59779888886d7bc0510e3f085d79708bd096de4
Fahadkhanreal <fhadikhan00@gmail.com>
2024-05-23 21:11:41 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,135 @@
+import { log } from "console";
+import inquirer from "inquirer";
+
+class student {
+  id: string;
+  name: string;
+  coursesEnrolled: string[];
+  feesAmount: number;
+
+  constructor(
+    id: string,
+    name: string,
+    coursesEnrolled: string[],
+    feesAmount: number
+  ) {
+    this.id = id;
+    this.name = name;
+    this.coursesEnrolled = coursesEnrolled;
+    this.feesAmount = feesAmount;
+  }
+}
+
+let baseId = 10000;
+let studentId: string = "";
+let continueEnrollment = true;
+
+let students: student[] = [];
+
+do {
+  let action = await inquirer.prompt({
+    type: "list",
+    name: "ans",
+    message: "please select an option\n",
+    choices: ["Enroll a student", "show student status"],
+  });
+
+  if (action.ans === "Enroll a student") {
+    let studentName = await inquirer.prompt({
+      type: "input",
+      name: "ans",
+      message: "please Enter your name:",
+    });
+
+    let trimmedStudentName = studentName.ans.trim().toLowerCase();
+    let studentNameCheck = students.map((obj) => obj.name);
+
+    if (studentNameCheck.includes(trimmedStudentName) === false) {
+      if (trimmedStudentName !== "") {
+        baseId++;
+        studentId = "STID" + baseId;
+
+        console.log("\n\tYour account has been created");
+        console.log(`welcome, ${trimmedStudentName}!`);
+
+        let course = await inquirer.prompt({
+          type: "list",
+          name: "ans",
+          message: "please select a course",
+          choices: ["IT", "English", "Cooking", "Physics"],
+        });
+
+        let courseFees = 0;
+        switch (course.ans) {
+          case "IT":
+            courseFees = 5000;
+            break;
+
+          case "English":
+            courseFees = 500;
+            break;
+
+          case "Cooking":
+            courseFees = 200;
+            break;
+
+          case "Physics":
+            courseFees = 1000;
+            break;
+        }
+
+        let courseConfirm = await inquirer.prompt({
+          type: "confirm",
+          name: "ans",
+          message: "Do you want to enroll in this course",
+        });
+
+        if (courseConfirm.ans === true) {
+          let Student = new student(
+            studentId,
+            trimmedStudentName,
+            [course.ans],
+            courseFees
+          );
+
+          students.push(Student);
+          console.log("You have enrolled in this course");
+        }
+      } else {
+        console.log("invalid Name");
+      }
+    } else {
+      console.log("this name is already available exists");
+    }
+  } else if (action.ans === "show student status") {
+    if (students.length !== 0) {
+      let studentNameCheck = students.map((e) => e.name);
+
+      let selectedstudents = await inquirer.prompt({
+        type: "list",
+        name: "ans",
+        message: "please select name",
+        choices: studentNameCheck,
+      });
+
+      let foundstudent = students.find(
+        (Student) => Student.name === selectedstudents.ans
+      );
+      console.log("student information");
+      console.log(foundstudent);
+      console.log("\n");
+    } else {
+      console.log("Record is empty");
+    }
+  }
+
+  let userConfirm = await inquirer.prompt({
+    type: "confirm",
+    name: "ans",
+    message: "Do you want to continue?",
+  });
+
+  if (userConfirm.ans === false) {
+    continueEnrollment = false;
+  }
+} while (continueEnrollment);
